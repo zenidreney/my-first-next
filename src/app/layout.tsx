03d7be9cf49d11d01cfe743d648425addbf2c227
Deniz <auo@tuta.io>
2026-01-23 17:06:15 +0100

    updated navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <nav className="flex justify-between">
-            <img src="/printforge-logo 1.png" />
-            <ul className="flex gap-3">
-              <li>3D MODELS</li>
-              <li>ABOUT</li>
+     <body>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4"> 
+            <div className="relative">
+              {/* Desktop logo */}
+              <img
+                src="/printforge-logo 1.png"
+                alt="PrintForge Logo"
+                className="w-[200px] h-auto hidden md:block"
+              />
+              {/* Mobile logo */}
+              <img
+                src="/printforge-logo-icon.svg"
+                alt="PrintForge Logo"
+                className="w-[40px] h-auto block md:hidden"
+              />
+            </div>
+            <ul className="flex items-center gap-2.5">
+              <p>3D Models</p>
+              <p>About</p>
             </ul>
-
           </nav>
         </header>
         {children}
