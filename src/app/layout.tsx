aaafb3628ea7a283cba21b36f642ff4b13ff4609
Deniz <auo@tuta.io>
2026-01-23 17:03:18 +0100

    added shared navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <header>
+          <nav className="flex justify-between">
+            <img src="/printforge-logo 1.png" />
+            <ul className="flex gap-3">
+              <li>3D MODELS</li>
+              <li>ABOUT</li>
+            </ul>
+
+          </nav>
+        </header>
         {children}
       </body>
     </html>
