a199d3d9ee8b7e5201055fda300bcdce05b96979
Deniz <auo@tuta.io>
2026-01-26 13:28:25 +0100

    started changing fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,28 @@
 import "./globals.css";
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log(albertSans);
   return (
     <html lang="en">
-     <body>
+      <body className={albertSans.className}>
         <header className="w-full bg-white">
-          <nav className="flex justify-between px-6 py-4"> 
+          <nav className="flex justify-between px-6 py-4">
             <div className="relative">
               {/* Desktop logo */}
               <img
