b0c376b69a4500d4a37719d673a3facb268b5f22
Deniz <auo@tuta.io>
2026-01-27 16:10:35 +0100

    added Next Link element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,56 @@
 import "./globals.css";
-import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
-const albertSans = Albert_Sans(
-  {
-    subsets: ["latin"],
-    display: "swap"
-  }
-)
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const montserratAlternates = Montserrat_Alternates({
   subsets: ["latin"],
   display: "swap",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-montserrat-alternates"
-})
+  variable: "--font-montserrat-alternates",
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log(albertSans)
+  console.log(albertSans);
   return (
     <html lang="en">
-     <body className={`${albertSans.className} ${montserratAlternates.variable}` } >
+      <body
+        className={`${albertSans.className} ${montserratAlternates.variable}`}
+      >
         <header className="w-full bg-white">
-          <nav className="flex justify-between px-6 py-4"> 
+          <nav className="flex justify-between px-6 py-4">
             <div className="relative">
-              {/* Desktop logo */}
-              <Image
-                src="/printforge-logo-1.png"
-                alt="PrintForge Logo"
-                width={200}
-                height={200}
-                className="w-[200px] h-auto hidden md:block"
-              />
-              {/* Mobile logo */}
-              <Image
-                src="/printforge-logo-mobile.png"
-                alt="PrintForge Logo"
-                width={200}
-                height={200}
-                className="w-[40px] h-auto block md:hidden"
-              />
+              <Link href={"/"}>
+                {/* Desktop logo */}
+                <Image
+                  src="/printforge-logo-1.png"
+                  alt="PrintForge Logo"
+                  width={200}
+                  height={200}
+                  className="w-[200px] h-auto hidden md:block"
+                />
+                {/* Mobile logo */}
+                <Image
+                  src="/printforge-logo-mobile.png"
+                  alt="PrintForge Logo"
+                  width={200}
+                  height={200}
+                  className="w-[40px] h-auto block md:hidden"
+                />
+              </Link>
             </div>
             <ul className="flex items-center gap-2.5">
-              <p>3D Models</p>
-              <p>About</p>
+              <li className="text-sm uppercase cursor-pointer"><Link href={"/3d-models"}>3D Models</Link></li>
+              <li className="text-sm uppercase cursor-pointer"><Link href={"/about"}>About</Link></li>
             </ul>
           </nav>
         </header>
